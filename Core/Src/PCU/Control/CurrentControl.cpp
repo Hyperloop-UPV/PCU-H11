1808c1ff612f6997bc1f1b8de4f76ec8dce3cceb
Cantonplas <dcancat@etsinf.upv.es>
2026-03-30 16:34:09 +0200

    Modulation frequency now is a slope

--- Core/Src/PCU/Control/CurrentControl.cpp
+++ Core/Src/PCU/Control/CurrentControl.cpp
@@ -32,7 +32,8 @@ double CurrentControl::calculate_frequency_modulation(){
     if(Comms::Reverse_direction){
         return exp_follower((a * -1 *PCU::control_data.speed_km_h_encoder) + b);
     }
-    return exp_follower((a * PCU::control_data.speed_km_h_encoder) + b);
+    // return exp_follower((a * PCU::control_data.speed_km_h_encoder) + b);
+    return (9.84f + 10.19f*SpaceVector::get_actual_time());
 
 }
 
